cc7f062d1cec12c9e20502cf9b44358fba45ebfa
SASAKI Shunsuke <erukiti@gmail.com>
2020-09-01 04:37:37 +0900

    日付TDD-3

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -4,7 +4,7 @@ describe('formatDate', () => {
   const now = new Date('2020-08-25 17:58:53')
   test('作成してすぐ', () => {
     expect(formatDate(new Date(now), now)).toEqual({
-      datetime: '2020/8/25 17:58:53',
+      datetime: '2020/08/25 17:58',
       isNew: true,
     })
   })
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -5,8 +5,27 @@ export type FormattedDate = {
 }
 
 export const formatDate = (d: Date, now: Date): FormattedDate => {
+  const dtf = new Intl.DateTimeFormat('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+  const [
+    { value: year },
+    ,
+    { value: month },
+    ,
+    { value: day },
+    ,
+    { value: hour },
+    ,
+    { value: minute },
+  ] = dtf.formatToParts(d)
+
   return {
-    datetime: '2020/8/25 17:58:53',
+    datetime: `${year}/${month}/${day} ${hour}:${minute}`,
     isNew: true,
   }
 }
