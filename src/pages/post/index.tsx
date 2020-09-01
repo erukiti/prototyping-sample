96cf2889dd947602d17a800ad9bdc33140fef403
SASAKI Shunsuke <erukiti@gmail.com>
2020-09-01 21:56:24 +0900

    投稿2 エディタautosize

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,12 +1,26 @@
 import { NextPage } from 'next'
+import React from 'react'
+import autosize from 'autosize'
 
 import styles from './index.module.css'
 
 const PostPage: NextPage = () => {
+  const ref = React.useRef<HTMLTextAreaElement>(null)
+
+  React.useEffect(() => {
+    if (ref.current) {
+      autosize(ref.current)
+    }
+  }, [])
+
   return (
     <div className={styles.editContent}>
       <input className={styles.subject} type="text" placeholder="タイトル" />
-      <textarea className={styles.editor} placeholder="本文を書きましょう" />
+      <textarea
+        className={styles.editor}
+        placeholder="本文を書きましょう"
+        ref={ref}
+      />
     </div>
   )
 }
