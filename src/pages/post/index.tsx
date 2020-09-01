fb3c7da5fb9248e243e03dd2a179992e5edd76b9
SASAKI Shunsuke <erukiti@gmail.com>
2020-09-02 03:04:37 +0900

    投稿7 投稿ボタンで投稿できるようにした

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,15 +1,20 @@
 import { NextPage } from 'next'
 import { useState, useCallback } from 'react'
+import { useRouter } from 'next/router'
 
 import { Editor } from '@/components/editor'
 import { SiteHeader, SiteHeaderItem } from '@/components/site-header'
 import { Button } from '@/components/button'
+import { usePostArticleMutation } from '@/generated/graphql'
 
 import styles from './index.module.css'
 
 const PostPage: NextPage = () => {
   const [subject, setSubject] = useState('')
   const [content, setContent] = useState('')
+  const [postArticle] = usePostArticleMutation()
+  const [postDisabled, setPostDisabled] = useState(false)
+  const router = useRouter()
 
   const handleChangeSubject = useCallback(
     (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,12 +23,43 @@ const PostPage: NextPage = () => {
     [],
   )
 
+  const handlePost = useCallback(
+    async (ev: React.FormEvent<HTMLFormElement>) => {
+      ev.preventDefault()
+      if (!content || !subject || postDisabled) {
+        return
+      }
+
+      setPostDisabled(true)
+      const { data } = await postArticle({
+        variables: {
+          // FIXME: authorIdはいったん決め打ちにする
+          authorId: '417b6cb6-718e-40f2-bf2a-ea8877ac688b',
+          content,
+          subject,
+          publishedAt: 'now()',
+        },
+      })
+      if (data && data.insert_articles_one) {
+        const aritcleId = data.insert_articles_one.id
+        // FIXME ユーザーID決め打ち
+        router.push(`/hoge/${aritcleId}`)
+        setPostDisabled(false)
+      } else {
+        console.log('POST unknown state', data)
+      }
+    },
+    [content, subject, postDisabled, postArticle, router],
+  )
+
   const siteHeaderRight = (
     <>
       <SiteHeaderItem>
-        <Button type="submit">
-          <span>投稿する</span>
-        </Button>
+        <form onSubmit={handlePost}>
+          <Button type="submit">
+            <span>投稿する</span>
+          </Button>
+        </form>
       </SiteHeaderItem>
       <SiteHeaderItem>
         <img className={styles.userIcon} src="/profile.png" />
